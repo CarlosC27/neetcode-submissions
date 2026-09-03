80289415467d6e5c540ddb3233639d8c5f2fe934
Carlos Carrillo-Sandoval <carlos.cs.2799@gmail.com>
2026-09-02 22:12:09 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type, float_type, bool_type, string_type, list_type = 13, 19.89, False, "Anti-Hero", [12, 27]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
