671eb81b329ebd8dd21720608991a75cedaa335f
Carlos Carrillo-Sandoval <carlos.cs.2799@gmail.com>
2026-09-02 22:13:47 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 13
+print(type(variable))
+variable = 19.89
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Issa"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
